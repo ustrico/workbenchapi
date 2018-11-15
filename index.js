08501bddc13c7a1edbc106f021e2c3add1496644
Alexey Fominih <site@marya.ru>
2018-11-15 10:14:15 +0400

    pg

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Client } = require('pg')
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
+})
+
+client.connect()
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row))
+  }
+  client.end()
+})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
